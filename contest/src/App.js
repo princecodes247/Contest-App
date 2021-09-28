bf7bdeccc116b6586d75a1417aaa99680d84c804
princecodes247 <princecodes247@gmail.com>
2021-09-28 03:01:44 -0400

    feat: add profile

--- contest/src/App.js
+++ contest/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { Profiler, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/login";
+import Profile from "./components/profile";
 import Home from "./components/home";
 import Signup from "./components/signup";
 import Contest from "./components/contest";
@@ -9,18 +10,23 @@ import ContestDetails from "./components/contestDetails";
 import BuyVotes from "./components/buyvotes";
 
 const App = () => {
+  const [token, setToken] = useState("");
+  // if(!token) {
+  //   return <Login setToken={setToken} />
+  // }
   return (
     <Router>
       <React.Fragment>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/login" exact component={Login}></Route>
-        <Route path="/signup" exact component={Signup}></Route>
-        <Route path="/contest" exact component={Contest}></Route>
+        <Route token={token} path="/" exact component={Home}></Route>
+        <Route token={token} path="/login" exact component={Login}></Route>
+        <Route token={token} path="/signup" exact component={Signup}></Route>
+        <Route token={token} path="/profile" exact component={Profile}></Route>
+        <Route token={token} path="/contest" exact component={Contest}></Route>
         <Route
-          path="/contest-details/:contestName/:contestPrize/:contestDate"
+          path="/contest-details/:contestID"
           component={ContestDetails}
         ></Route>
-        <Route path="/buy-votes/" exact component={BuyVotes}></Route>
+        <Route path="/buy-votes/:contestID" component={BuyVotes}></Route>
       </React.Fragment>
     </Router>
   );
---
+++ contest/src/components/profile.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import "../styles/login.css";
+import "../styles/profile.css";
+import { Link } from "react-router-dom";
+import img1 from "../assets/wave.svg";
+import img2 from "../assets/zhanarys-dakhiyev-WMlRkqt1vII-unsplash.jpg";
+import FileBase64 from 'react-file-base64';
+
+const Profile = () => {
+	
+
+	const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+	const [newPassword, setNewPassword] = useState("");
+	const [password, setPassword] = useState("");
+  const [passwordAlert, setPasswordAlert] = useState({text:"", class: "red-text"});
+    const [profilePic, setProfilePic] = useState("");
+
+
+    const checkPasswords = () => {
+      if(password == "" && newPassword == "") {
+        setPasswordAlert({text:"", class: "red-text"})
+      }
+      if (password == newPassword) {
+        setPasswordAlert({text:"Passwords match", class: "green-text"})
+      } else {
+        setPasswordAlert({text:"Passwords do not match", class: "red-text"})
+      }
+    }
+	const handleChange = (e) => {
+		setUsername(e.target.value);
+	};
+	const handleChange2 = (e) => {
+		setEmail(e.target.value);
+	  };
+    const handleChange3 = (e) => {
+      setPassword(e.target.value);
+      checkPasswords()
+      };
+      const handleChange4 = (e) => {
+        setNewPassword(e.target.value);
+        checkPasswords()
+        };
+	const handleSubmit = async e => {
+		e.preventDefault();
+	
+	  }
+      const handleProfilePic = async e => {
+          console.log(e);
+        setProfilePic(e.base64)
+      }
+  return (
+    <React.Fragment>
+		  <form onSubmit={handleSubmit} className="container reduce-form">
+			  <h1> Profile </h1>
+            <div class="profile-pic-input profile-adjust">
+                <span className="profile-cover">
+                <div className="image-cropper profile-pic-cont" style={{background: `url(${profilePic}) no-repeat 50% 50%`}}>
+                {/* <img className="profile-pic" src={profilePic} /> */}
+                </div>
+                <label className="file-input">
+                <FileBase64 multiple={false} onDone={handleProfilePic}/>
+                </label>
+                </span>
+                <button className="save-btn">Save Picture</button>
+            </div>
+                <div className="input-field">
+				  <h2> Change Username</h2>
+				  <span> <i className="fa fa-user"></i>  <input type="text" placeholder=" Type your Username"  value={username}
+                    onChange={handleChange} /></span>
+            <button className="save-btn">Save Username</button>
+            </div>
+            <div className="input-field">
+            <h2> Change Email Address</h2>
+            <span> <i className="fa fa-user"></i>  <input type="text" placeholder=" Type your Email"  value={email}
+            onChange={handleChange2} /></span>
+            <button className="save-btn">Save Email</button>
+            </div>
+            <div className="input-field">
+				  <h2> Change Password</h2>
+				  <span> <i className="fa fa-lock"></i> <input type="password" placeholder=" Old Password"  value={password}
+            onChange={handleChange3}/> </span>
+            <span> <i className="fa fa-lock"></i> <input type="password" placeholder=" New Password"  value={newPassword}
+            onChange={handleChange4}/> </span>
+            <p className={passwordAlert.class}>{passwordAlert.text}</p>
+            <button className="save-btn">Save Password</button>
+                </div>
+            
+			
+
+			  
+			  
+
+			  {/* <p className="padding"> If you don't have an account, you can <Link to="/signup"> sign up here </Link></p> */}
+			  <img className="box-wave" src={img1}/>
+			</form>
+    </React.Fragment>
+  );
+};
+
+export default Profile;
